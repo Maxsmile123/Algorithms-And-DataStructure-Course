f05e3724b2f827b7cd7a54c8eb3faf2b3d549037
IRBXI <rbxrbx@mail.ru>
2025-03-02 13:29:23 +0300

    fixed formatting

--- tasks/lists/list/exceptions.hpp
+++ tasks/lists/list/exceptions.hpp
@@ -3,10 +3,10 @@
 #include <exception>
 #include <string>
 
-class ListIsEmptyException: std::exception {
+class ListIsEmptyException : std::exception {
 public:
-    explicit ListIsEmptyException(const std::string& text):
-        error_message_(text){}
+    explicit ListIsEmptyException(const std::string& text) : error_message_(text) {
+    }
 
     const char* what() const noexcept override {
         return error_message_.data();
